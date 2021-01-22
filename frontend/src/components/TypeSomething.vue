9585a42c7467e46c911e5840400229e18cc48794
Roman <useflame@Romans-MacBook-Air.local>
2021-01-22 05:20:58 +0300

    vue setup

---
+++ frontend/src/components/TypeSomething.vue
@@ -0,0 +1,18 @@
+<template>
+<textarea></textarea>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "TypeSomething",
+  props: {
+    msg: String
+  }
+});
+</script>
+
+<style scoped>
+
+</style>
