c695b9ed325c08c19c97c9df37c620f7581e974c
Roman <useflame@Romans-MacBook-Air.local>
2021-01-25 20:32:16 +0300

    add onSpace func

---
+++ frontend2/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <TypeSomething :text="text" :writtenText="writtenText"/>
+  </div>
+</template>
+
+<script>
+import TypeSomething from './components/TypeSomething.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TypeSomething
+  },
+  data() {
+    return {
+      text: 'The responses to the pandemic have resulted in global social and economic disruption, including the largest global recession since the Great Depression. It has led to the postponement or cancellation of events, widespread supply shortages exacerbated by panic buying, agricultural disruption and food shortages, and decreased emissions of pollutants and greenhouse gases. Many educational institutions have been partially or fully closed. Misinformation has circulated through social media and mass media. There have been incidents of xenophobia and discrimination against Chinese people and against those perceived as being Chinese or as being from areas with high infection rates.',
+      writtenText: 0
+    };
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ frontend2/src/components/TypeSomething.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="container w-75">
+    <div class="row">
+      <div class ="col">
+      <p>{{text}}</p>
+    </div>
+    </div>
+    <div class="row">
+      <div class="col">
+      <input
+        @keyup.space="onSpace"
+        v-model="typeField"
+        autocomplete="off"
+        spellcheck="false"
+        @keyup="onChange"
+        class="w-100"
+        autofocus
+        rows="1"
+        />
+        <h1>{{writtenText}}</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+
+
+<script>
+
+export default {
+  name: 'TypeSomething',
+  props: {
+    text: String,
+    typeField: String,
+    writtenText: Number,
+  },
+  methods:{ 
+    onSpace() {
+      let s = this.text.substring(this.writtenText, this.writtenText + this.typeField.length);
+      if(this.typeField == s){
+        this.writtenText += this.typeField.length
+        this.typeField = "";
+        
+      }
+    },
+    onChange(){
+    },
+  }
+}
+</script>
+
+
+<style scoped>
+p {
+  text-align: left;
+  padding: 0;
+  /*Turn off copying text*/
+  -webkit-user-select: none;
+  -khtml-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  -o-user-select: none;
+  user-select: none;
+}
+.right {
+  color: green;
+}
+.wrong {
+  color: red;
+}
+</style>
+
