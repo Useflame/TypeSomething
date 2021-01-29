af4982cadd56c4c7906a9f6f2659f272adbdabfa
Roman <useflame@Romans-MacBook-Air.local>
2021-01-30 02:06:02 +0300

    typing window is ready

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <TypeSomething :text="text" :writtenText="writtenText"/>
+    <TypeSomething :text="text" :greenText="greenText" :word="word" :bg="bg"/>
   </div>
 </template>
 
@@ -15,7 +15,12 @@ export default {
   data() {
     return {
       text: 'The responses to the pandemic have resulted in global social and economic disruption, including the largest global recession since the Great Depression. It has led to the postponement or cancellation of events, widespread supply shortages exacerbated by panic buying, agricultural disruption and food shortages, and decreased emissions of pollutants and greenhouse gases. Many educational institutions have been partially or fully closed. Misinformation has circulated through social media and mass media. There have been incidents of xenophobia and discrimination against Chinese people and against those perceived as being Chinese or as being from areas with high infection rates.',
-      writtenText: ''
+      greenText: '',
+      word: '',
+      bg: {
+        color: 'grey',
+        backgroundColor: ''
+      }
     };
 
   }
--- frontend/src/components/TypeSomething.vue
+++ frontend/src/components/TypeSomething.vue
@@ -4,26 +4,28 @@
       <div class ="col">
       
         <div class="type-field">
-          <span class="right">{{ text.substring(0, writtenText.length) }}</span>
-          <span class="wrong"></span>
-          <span style="text-decoration: underline">
-          {{ text.substring(writtenText.length).split(' ').slice(0, 1)[0]}}
-          </span>
-          <span v-for="word in text.substring(writtenText.length).split(' ').slice(1)" :key ="word">{{ word + ' ' }}</span>
+          <span style="color: green;">{{ greenText }}</span>
+          <span style="text-decoration: underline;"> {{ text.substring(greenText.length).split(' ')[0] }}</span>
+          <span v-for="word in text.substring(greenText.length).split(' ').slice(1)"
+          :key="word"
+          >
+        {{ word }}
+      </span>
         </div>
     </div>
     </div>
     <div class="row">
       <div class="col">
       <input
-
+        @keyup.space="onSpace"
+        @keyup="onChange"
         v-model="typeField"
         autocomplete="off"
         spellcheck="false"
-        @keyup="onSpace"
         class="w-100"
         autofocus
         rows="1"
+        :style="bg"
         />
       </div>
     </div>
@@ -34,28 +36,33 @@
 
 
 <script>
-import Vue from 'vue';
-import TextHighlight from 'vue-text-highlight';
-
-Vue.component('text-highlight', TextHighlight);
-
-
 export default {
   name: 'TypeSomething',
   props: {
     text: String,
     typeField: String,
-    writtenText: String,
+    greenText: String,
+    bg: Object,
   },
   methods:{ 
     onSpace() {
-      let s = this.text.substring(this.writtenText.length, this.writtenText.length + this.typeField.length);
-      if(this.typeField == s){
-        this.writtenText += this.typeField
+      let s = this.text.substring(this.greenText.length, this.greenText.length + this.typeField.length);
+      if((this.typeField.trim() == s.trim())){
+        this.greenText += this.typeField
         this.typeField = "";
+      } else if(this.text == this.greenText) {
+        alert('1234')
       }
     },
     onChange(){
+      let s = this.text.substring(this.greenText.length, this.greenText.length + this.typeField.length);
+      if((this.typeField.trim() != s.trim())){
+        this.bg.backgroundColor = 'red'
+        this.bg.color='black'
+      } else {
+        this.bg.backgroundColor = ''
+        this.bg.color = 'grey'
+      }
     },
   }
 }
@@ -73,12 +80,6 @@ export default {
   -ms-user-select: none;
   -o-user-select: none;
   user-select: none;
-}
-.right {
-  color: green;
-}
-.wrong {
-  color: red;
 }
 </style>
 
